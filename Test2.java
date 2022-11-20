6f8669a0caeb7f63e803f53615d9d38958da1f29
xuezao105 <xuezao105@gmail.com>
2022-11-20 17:12:55 +0900

    test

---
+++ Test2.java
@@ -0,0 +1,8 @@
+public class Test2 {
+	public static void main(String args[]) {
+		String name = "James";
+		// following will return true since name is type of String;
+		boolean result = name instanceof String;
+		System.out.println(result);
+	}
+}
