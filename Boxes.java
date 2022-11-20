847125075f869f50aa4d82abea9da43ca421a4f6
xuezao105 <xuezao105@gmail.com>
2022-11-20 21:59:13 +0900

    test

---
+++ Boxes.java
@@ -0,0 +1,9 @@
+public class Boxes {
+	public static void main(String args[]) {
+		Integer x = 5;
+		// boxer int to an Integer object x = x + 10;
+		// unboxes the Integer to a int 
+		System.out.println(x);
+		
+	}
+}
