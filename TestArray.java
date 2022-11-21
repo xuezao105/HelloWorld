e12a77b32bf719fc615df8b022406295096c503e
xuezao105 <xuezao105@gmail.com>
2022-11-21 20:47:54 +0900

    test

---
+++ TestArray.java
@@ -0,0 +1,24 @@
+public class TestArray {
+	public static void main(String[] args) {
+		double[]  myList = { 
+				2.3,4.5,5.6,7.6
+		};
+		// Print all the array elements
+		for(int i = 0;i < myList.length;i++) {
+			System.out.println(myList[i] + " ");
+		}
+		// Summing all elements
+		double total = 0;
+		for(int i = 0;i < myList.length;i++) {
+			total += myList[i];
+		}
+		System.out.println("Total is " + total);
+		// Finding the largest element
+		double max = myList[0];
+		for(int i = 1;i < myList.length;i++) {
+			if(myList[i] > max)max = myList[i];
+		}
+		System.out.println("Max is " + max);
+	}
+	
+}
