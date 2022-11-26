34ff0666e34695fc0130400219a463fcd962628a
xuezao105 <xuezao105@gmail.com>
2022-11-26 22:03:14 +0900

    test

---
+++ LanguageFacts.java
@@ -0,0 +1,9 @@
+public class LanguageFacts {
+	public static void main(String[] args) {
+		// Entering a newline after the first semicolon
+		System.out.println("Java is a clssbased language.");
+		System.out.println("Java classes have a'main'method.");
+		System.out.println("Java statements end with a semicolon");
+		System.out.println("Programming is...");
+	}
+}
