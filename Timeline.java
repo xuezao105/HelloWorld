ff3c5c0e6220df0d64740ad89d09e4f970378791
xuezao105 <xuezao105@gmail.com>
2022-11-26 18:34:14 +0900

    test

---
+++ Timeline.java
@@ -0,0 +1,14 @@
+// A tale of sound and fury
+
+public class Timeline {
+	public static void main(String[] args) {
+		/**
+		 * 这人很自恋。
+		 */
+		System.out.println("James Gosling is a Canadian");
+		System.out.println("engineer who created Java while");
+		System.out.println(" working at Sun Microsystems.");
+		System.out.println("His favorite number is the square root of 2!");
+	}
+	
+}
