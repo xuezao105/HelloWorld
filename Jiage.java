363b7056e395e7b2907009115f183dfe4565eace
xuezao105 <xuezao105@gmail.com>
2022-11-27 15:51:44 +0900

    test

---
+++ Jiage.java
@@ -0,0 +1,7 @@
+public class Jiage {
+	public static void main(String[] args) {
+		System.out.println("书名\t作者\t价格\t销量");
+		System.out.println("\n");
+		System.out.println("三国\t罗贯中\t120\t1000");
+	}
+}
