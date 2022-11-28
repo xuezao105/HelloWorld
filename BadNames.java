0f5955c560a49cdc240a2759dffe916247ffbb4a
xuezao105 <xuezao105@gmail.com>
2022-11-28 21:26:55 +0900

    Badname

---
+++ BadNames.java
@@ -0,0 +1,10 @@
+public class BadNames {
+	public static void main(String[] args) {
+		String Name1 = "Sarila";
+		String Name2 = "Smith";
+		String com   = "Sarila.google.com";
+		int salaryexecption = 10000;
+		int yearofbirth = 1955;
+		System.out.println("The program runs!");
+	}
+}
