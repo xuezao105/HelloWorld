ff1d039f914191b86737a0af9a664b348ccc3a0a
xuezao105 <xuezao105@gmail.com>
2022-11-29 21:52:03 +0900

    int

---
+++ GuessingGame.java
@@ -0,0 +1,14 @@
+public class GuessingGame {
+	public static void main(String[] args) {
+		// declare initial 
+		//balance double balance = 20000.99;
+		// declare deposit amount
+	       //double depositAmount = 1000.00;
+		// store result of calculation in our original variable
+		//balance = balance + depositAmount;
+		
+		int mystery1 = 8 + 6;
+		int mystery2 = 8 - 6;
+		System.out.println(mystery2);
+	}
+}
