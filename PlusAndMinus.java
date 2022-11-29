0f37b8c6f017996f4b5b39ae51f513fb215b1bd6
xuezao105 <xuezao105@gmail.com>
2022-11-29 22:10:27 +0900

    plus

---
+++ PlusAndMinus.java
@@ -0,0 +1,24 @@
+public class PlusAndMinus {
+	public static void main(String[] args) {
+		/*
+		double balance = 20000.99;
+		double depositAmount = 1000.0;
+		balance = balance + depositAmount;
+		// balance now holds 21000.99
+		double withdrawAmount = 500;
+		balance = balance - withdrawAmount;
+		// balance now holds 19500.99
+		 * int numPicturesOfCats = 60 + 24;
+		  Take a picture numPicturesOfCats++ // Value is now 85
+ // Delete a picture numPicturesOfCats-- // Value is now 84
+		 */
+	        int zebrasInZoo = 8;
+		    int giraffesInZoo = 4;
+		    int animalsInZoo = zebrasInZoo + giraffesInZoo;
+		    System.out.println(animalsInZoo);
+		    int numZebraAfterTrade = zebrasInZoo - 2;
+		    System.out.println(numZebraAfterTrade);
+			}
+		
+		
+	}
