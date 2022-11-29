fed753ee7b07b24e9247ad71aba363188e6dfc59
xuezao105 <xuezao105@gmail.com>
2022-11-29 13:28:26 +0900

    char

---
+++ Char01.java
@@ -0,0 +1,16 @@
+//演示char的基本使用
+public class Char01 {
+	//编写一个main的方法
+	public static void main(String[] args) {
+		char c1 = 'a';
+		char c2 = '\t';
+		char c3 = '韩';
+		char c4 = 97;//说明：字符类型可以直接存放一个数字
+		System.out.println(c1);
+		System.out.println(c2);
+		System.out.println(c3);
+		System.out.println(c4);
+		
+				
+	}
+}
