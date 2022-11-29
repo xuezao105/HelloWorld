9eb2fd393b2bd76cce07cc8123ceedc52adfd9a6
xuezao105 <xuezao105@gmail.com>
2022-11-29 21:31:48 +0900

    madlib

---
+++ MadLib.java
@@ -0,0 +1,37 @@
+public class MadLib {
+  /*
+  **Your description here
+  This program generates a mad
+  Limbed story.
+  Author: aura
+  Date:2/19/2049
+  */
+  	public static void main(String[] args){
+      
+      
+      
+      //The template for the story
+  		String story = "This morning name1 woke up feeling adjective1. It is going to be a adjective2 day! Outside, a bunch of noun1 were protesting to keep noun2 in stores. They began to verb1 to the rhythm of the noun3, which made all the noun4 very adjective3. Concerned, name1 texted name2, who flew name1 to place1 and dropped name1 in a puddle of frozen noun5. name1 woke up in the year number, in a world where noun6 ruled the world.";
+        
+  		
+  		
+      String adjective1 = "cold";
+      String adjective2 = "good";
+      String adjective3 = "bad";
+      String verb1 = "catch";
+      String noub1 = "Smith";
+      String noub2 = "Tommy";
+      String noub3 = "Window";
+      String noub4 = "Song";
+      String noub5 = "bool";
+      String noub6 = "football";
+      String name2 = "Bush";
+      int number = 6;
+      String place1 ="kasai";
+      System.out.println(story);
+
+
+      
+    
+    }       
+}
