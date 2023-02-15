004b78994d21fa8fcf36c96165401162ea41596f
xuezao105 <xuezao105@gmail.com>
2023-02-15 17:52:55 +0900

    toLowerCase,toUpperCase

---
+++ Hashtag.java
@@ -0,0 +1,14 @@
+public class Hashtag {
+	
+	public static void main(String[] args) {
+		
+		String hashtag = "#100DaysOfCode";
+		
+		// Make the hashtag Lowercase;
+		
+		System.out.println(hashtag.toLowerCase());
+		
+		System.out.println(hashtag.toUpperCase());
+	}
+	
+}
