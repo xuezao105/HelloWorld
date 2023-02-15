2f551b92a9b8660ea5d924fc49e11a2a671ab6d4
xuezao105 <xuezao105@gmail.com>
2023-02-15 17:34:33 +0900

    substring

---
+++ Poetry.java
@@ -0,0 +1,9 @@
+public class Poetry {
+	
+	public static void main(String[] args) {
+		
+		String line = "白日依山尽，黄河入海流。";
+		// Change the arguments:
+		System.out.println(line.substring(3,8));
+	}
+}
